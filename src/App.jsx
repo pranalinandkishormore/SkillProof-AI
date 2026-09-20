30e2d6893774eb937574adce9f5711c49302c09e
pranalinandkishormore <pranalimore1221@gmail.com>
2026-09-20 21:34:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,3 @@
-import Quiz from "./Quiz";
-import CodeEditor from "./CodeEditor";
-
-function App() {
-  return (
-    <div>
-      <Quiz />
-      <CodeEditor />
-    </div>
-  );
-}
-
-export default App;
-import Quiz from "./Quiz";
 import React from 'react';
 import StudentDashboard from './StudentDashboard';
 
